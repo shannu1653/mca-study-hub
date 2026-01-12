bfa2538f2b2ffb9642f672703b188d5ba2b1a6f1
shannu1653 <pentashanmukha2002@gmail.com>
2026-01-12 20:47:11 +0530

    Add password reset token model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -37,3 +37,19 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+
+from django.conf import settings
+import uuid
+
+class PasswordResetToken(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    token = models.UUIDField(default=uuid.uuid4, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.email} - {self.token}"
