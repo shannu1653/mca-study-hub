6162ab796c5ff3a3dcea4bb18858d5d7774be936
shannu1653 <pentashanmukha2002@gmail.com>
2026-02-02 17:37:10 +0530

    changes layout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import App from "./App";
 import "./index.css";
 import "./styles/main.css";
-import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Toaster position="top-center" reverseOrder={false} />
+
+      {/* ✅ Mobile-safe Toaster */}
+      <Toaster
+        position="top-center"
+        containerStyle={{
+          top: 20,
+          pointerEvents: "none",   // 🔥 IMPORTANT
+        }}
+        toastOptions={{
+          style: {
+            pointerEvents: "auto",
+          },
+        }}
+      />
     </BrowserRouter>
   </React.StrictMode>
 );
