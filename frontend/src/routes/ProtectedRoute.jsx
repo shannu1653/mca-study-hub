37a9c8ea64b0f46823a7f2d05d7e05111a161c9c
shannu1653 <pentashanmukha2002@gmail.com>
2026-02-02 22:47:05 +0530

    changes protected routes

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("access");
+  const token = localStorage.getItem("access_token");
 
   if (!token || token === "undefined" || token === "null") {
     return <Navigate to="/login" replace />;
