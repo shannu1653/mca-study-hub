e95ec23682503d0212c03a0a84b91503e47e77fe
shannu1653 <pentashanmukha2002@gmail.com>
2026-02-02 10:20:15 +0530

    fix error

--- frontend/src/pages/admin/ManageNotes.jsx
+++ frontend/src/pages/admin/ManageNotes.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import api from "../../api/axios";
 import Layout from "../../layout/Layout";
 import toast from "react-hot-toast";
-import "../../styles/manageNotes.css";
+import "../../styles/ManageNotes.css";
 
 
 function ManageNotes() {
